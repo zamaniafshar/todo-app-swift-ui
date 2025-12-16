e39106e3ed38cfceed485ee3b5ea19b536162458
Amin Zamani Afshar <zamani35099@gmail.com>
2025-12-16 22:31:13 +0330

    Initial Commit

---
+++ swift_ui_practice/swift_ui_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_practiceApp.swift
+//  swift_ui_practice
+//
+//  Created by Mac on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
